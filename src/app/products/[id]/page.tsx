303b23e41b7eb410c062d0e0f04151298afc1d12
GoldenAzusa <101515269+nxh1604@users.noreply.github.com>
2024-02-01 22:36:22 +0700

    laptop commit

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -9,7 +9,15 @@ import productsData from "@/lib/data/productsData";
 import clsx from "clsx";
 import Image from "next/image";
 import { useContext } from "react";
-import { FaFacebook, FaFacebookMessenger, FaHeart, FaPinterest, FaRegHeart, FaTruck, FaTwitter } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaFacebookMessenger,
+  FaHeart,
+  FaPinterest,
+  FaRegHeart,
+  FaTruck,
+  FaTwitter,
+} from "react-icons/fa";
 import { GiShoppingCart } from "react-icons/gi";
 import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from "react-icons/md";
 
@@ -46,33 +54,63 @@ const Page = ({ params }: { params: { id: string } }) => {
               width={400}
               height={400}
               src={src}
-              alt=""
-            ></img>
+              alt=""></img>
             <div className="relative mt-4">
               <button
                 aria-label="previous image"
-                className="text-white absolute left-0 top-[50%] hover:bg-black/50 translate-y-[-50%] bg-black/5 py-2"
-              >
+                className="text-white absolute left-0 top-[50%] hover:bg-black/50 translate-y-[-50%] bg-black/5 py-2">
                 <MdOutlineKeyboardArrowLeft className="w-6 h-6" />
               </button>
               <ul className="flex gap-2 *:w-1/5">
-                <li className="border-red-500 border-4">
-                  <img src={product.src} alt="" width={150} height={150} className="w-full object-contain" />
+                <li className="border-red-500 border-2">
+                  <img
+                    src={product.src}
+                    alt=""
+                    width={150}
+                    height={150}
+                    className="h-[80px] object-contain"
+                  />
                 </li>
                 <li>
-                  <img src="https://placewaifu.com/image/300" alt="" width={150} height={150} className="w-full object-contain" />
+                  <img
+                    src="https://placewaifu.com/image/300"
+                    alt=""
+                    width={150}
+                    height={150}
+                    className="h-[80px] object-contain"
+                  />
                 </li>
                 <li>
-                  <img src="https://placewaifu.com/image/300" alt="" width={150} height={150} className="w-full object-contain" />
+                  <img
+                    src="https://placewaifu.com/image/300"
+                    alt=""
+                    width={150}
+                    height={150}
+                    className="h-[80px] object-contain"
+                  />
                 </li>
                 <li>
-                  <img src="https://placewaifu.com/image/300" alt="" width={150} height={150} className="w-full object-contain" />
+                  <img
+                    src="https://placewaifu.com/image/300"
+                    alt=""
+                    width={150}
+                    height={150}
+                    className="h-[80px] object-contain"
+                  />
                 </li>
                 <li>
-                  <img src="https://placewaifu.com/image/300" alt="" width={150} height={150} className="w-full object-contain" />
+                  <img
+                    src="https://placewaifu.com/image/300"
+                    alt=""
+                    width={150}
+                    height={150}
+                    className="h-[80px] object-contain"
+                  />
                 </li>
               </ul>
-              <button aria-label="next image" className="text-white absolute  hover:bg-black/50 right-0 top-[50%] translate-y-[-50%] bg-black/5 py-2">
+              <button
+                aria-label="next image"
+                className="text-white absolute  hover:bg-black/50 right-0 top-[50%] translate-y-[-50%] bg-black/5 py-2">
                 <MdOutlineKeyboardArrowRight className="w-6 h-6" />
               </button>
             </div>
@@ -101,16 +139,27 @@ const Page = ({ params }: { params: { id: string } }) => {
             <h2 className="sr-only">Product Information Section</h2>
             <div>
               <div className="font-[500] pb-2 mt-[3px]">
-                <span className="mr-3 bg-primary text-white p-1 rounded-sm text-xs relative -top-[3px] capitalize">yêu thích</span>
+                <span className="mr-3 bg-primary text-white p-1 rounded-sm text-xs relative -top-[3px] capitalize">
+                  yêu thích
+                </span>
                 <div className="inline text-xl leading-[0.75rem]">{product.title}</div>
               </div>
               <div className="flex gap-8 pb-4 mt-[10px]">
                 <div className="flex gap-1 items-center">
-                  <span className="text-primary border-b-2 border-b-primary tracking-wider">{product.rating}</span>
-                  <RatingStar starSize={16} starGap={4} numOfStars={5} ratingStar={product.rating} className="relative -top-[1px]" />
+                  <span className="text-primary border-b-2 border-b-primary tracking-wider">
+                    {product.rating}
+                  </span>
+                  <RatingStar
+                    starSize={16}
+                    starGap={4}
+                    numOfStars={5}
+                    ratingStar={product.rating}
+                    className="relative -top-[1px]"
+                  />
                 </div>
                 <div className="capitalize flex gap-1 items-center">
-                  <span className="tracking-wider border-b-2 border-black">1,1k</span> <span className="text-black/70">Đánh Giá</span>
+                  <span className="tracking-wider border-b-2 border-black">1,1k</span>{" "}
+                  <span className="text-black/70">Đánh Giá</span>
                 </div>
                 <div className="capitalize flex gap-1 items-center">
                   <span className="tracking-wider">{sold}</span>
@@ -127,18 +176,37 @@ const Page = ({ params }: { params: { id: string } }) => {
                 <span className="text-3xl text-primary">
                   <Currency price={product.price - (product.price * product.discount) / 100} />
                 </span>
-                <div className="uppercase bg-red-500 text-white px-1 rounded-sm text-xs font-bold">{product.discount}% giảm</div>
+                <div className="uppercase bg-red-500 text-white px-1 rounded-sm text-xs font-bold">
+                  {product.discount}% giảm
+                </div>
               </div>
             </div>
 
-            <div className={clsx("pl-3 flex-1 flex flex-col pb-[30px] text-sm", sections.length < 3 ? "justify-around" : "justify-between")}>
+            <div
+              className={clsx(
+                "pl-3 flex-1 flex flex-col pb-[30px] text-sm",
+                sections.length < 3 ? "justify-around" : "justify-between"
+              )}>
               {sections.map((section) => (
-                <section className={clsx("flex", section.section.toLowerCase() !== "vận chuyển" && "items-center")} key={section.section}>
-                  <h3 className="w-[110px] capitalize leading-tight text-[#757575]">{section.section}</h3>
+                <section
+                  className={clsx(
+                    "flex",
+                    section.section.toLowerCase() !== "vận chuyển" && "items-center"
+                  )}
+                  key={section.section}>
+                  <h3 className="w-[110px] capitalize leading-tight text-[#757575]">
+                    {section.section}
+                  </h3>
                   {section.section.toLowerCase() === "vận chuyển" && (
                     <div className="space-y-2">
                       <div className="flex gap-2 items-center">
-                        <Image className="w-[30px] h-[30px] object-contain" width={50} height={50} alt="" src="/img/freeship.png" />
+                        <Image
+                          className="w-[30px] h-[30px] object-contain"
+                          width={50}
+                          height={50}
+                          alt=""
+                          src="/img/freeship.png"
+                        />
                         <p>Miễn phí vận chuyển</p>
                       </div>
                       <div className="flex gap-2 items-center">
@@ -157,7 +225,10 @@ const Page = ({ params }: { params: { id: string } }) => {
                     <div className="flex items-center gap-4">
                       <div className="flex">
                         <button className="w-[30px] border-[1px]">-</button>
-                        <input className="text-center border-[1px] w-[50px] py-2" defaultValue={1} />
+                        <input
+                          className="text-center border-[1px] w-[50px] py-2"
+                          defaultValue={1}
+                        />
                         <button className="w-[30px] border-[1px]">+</button>
                       </div>
                       <span>X sản phẩm có sẵn</span>
@@ -172,7 +243,9 @@ const Page = ({ params }: { params: { id: string } }) => {
                 <GiShoppingCart className="w-6 h-6" />
                 Thêm vào giỏ hàng
               </button>
-              <button className="text-white w-[200px] bg-red-700 hover:opacity-90 px-4 py-3">Mua ngay</button>
+              <button className="text-white w-[200px] bg-red-700 hover:opacity-90 px-4 py-3">
+                Mua ngay
+              </button>
             </div>
             <div className="pl-3 mt-[30px]">Footer</div>
           </section>
